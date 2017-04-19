46c771857cc36d75f320c978e6e157cd35e4b633
WANGQINGCHUN <WANGQINGCHUN@kingsoft.com>
2017-04-19 22:21:03 +0800

    more code

--- src/Config/Ini.php
+++ src/Config/Ini.php
@@ -11,5 +11,63 @@ namespace Spf\Framework\Config;
 
 class Ini extends AbstractConfig
 {
-    
+    protected $nestSeparator = '.';
+
+    protected $sectionSeparator = ':';
+
+    protected $skipExtends = false;
+
+    public function __construct($filename, $section = null, $options = false)
+    {
+        if (empty($filename)) {
+            throw new ConfigException('Filename is not set');
+        }
+
+        $allowInMemoryModifications = false;
+        if (is_bool($options)) {
+            $allowInMemoryModifications = $options;
+        } else if (is_array($options)) {
+            if (isset($options['allowInMemoryModification'])) {
+                $allowInMemoryModifications = (boolean)$options['allowInMemoryModification'];
+            }
+            if (isset($options['nestSeparator'])) {
+                $this->nestSeparator = $options['nestSeparator'];
+            }
+
+            if (isset($options['skipExtends'])) {
+                $this->skipExtends = (boolean)$options['skipExtends'];
+            }
+        }
+
+        $iniArray = $this->loadIniFile($filename);
+
+        if (null === $section) {
+            $dataArray = [];
+        }
+    }
+
+    protected function loadIniFile($filename)
+    {
+        $loaded = $this->parseIniFile($filename);
+        $iniArray = [];
+        foreach ($loaded as $key => $data) {
+            $pieces = explode($this->sectionSeparator, $key);
+            $thisSection = trim($pieces[0]);
+            switch (count($pieces)) {
+                case 1:
+                    $iniArray[$thisSection] = $data;
+                    break;
+
+                case 2:
+                    $extendedSection = trim($pieces[1]);
+                    $iniArray[$thisSection] = array_merge([';extends' => $extendedSection], $data);
+                    break;
+
+                default:
+                    throw new ConfigException("Section '$thisSection' may not exist");
+            }
+        }
+
+        return $iniArray;
+    }
 }
